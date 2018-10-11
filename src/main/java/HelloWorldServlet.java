ddbf89c84551c255f298e02af763d9584f45d985
Stephanie Velazquez <svelazquez@Stephanies-MacBook-Air.local>
2018-10-11 10:54:33 -0500

    servlet exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,54 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.IOException;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello", "/counter"})
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    private int hitCount;
+
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+        throws IOException {
+        //headers
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        //String name = "World";
+        String name = req.getParameter("name");
+        //parameter handling
+        if( name == null)
+            name = "World";
+
+
+        //print hello
+        out.println("Hello " + name);
+
+
+
+        String reset = req.getParameter("reset");
+
+
+        if(reset == null){
+            hitCount++;
+            out.println("Counter: " + hitCount);
+        } else if(reset.equals("true")){
+            hitCount=0;
+        }
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
